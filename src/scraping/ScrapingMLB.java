453db688a5311167c0c41b896c11c8253549f0fe
yfsanchez <yfsanchez@gmail.com>
2019-08-08 14:34:41 -0300

    agregar carpeta
    Agregando la carpeta para almacenar los xml

--- src/scraping/ScrapingMLB.java
+++ src/scraping/ScrapingMLB.java
@@ -85,7 +85,8 @@ public class ScrapingMLB {
 		TransformerFactory transformerFactory = TransformerFactory.newInstance();
 		Transformer transformer = transformerFactory.newTransformer();
 		DOMSource source = new DOMSource(doc);
-		StreamResult result = new StreamResult(new File(nombreFichero + ".xml"));
+		String ruta = "dataXML\\";
+		StreamResult result = new StreamResult(new File(ruta, nombreFichero + ".xml"));
 
 		// StreamResult result = new StreamResult(new File("archivo.xml"));
 		// Si se quiere mostrar por la consola...
